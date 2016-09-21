256fd883d392148623e68bcc1366abcb955024d8
Darin Holbrook <darinholbrook@gmail.com>
2016-09-21 13:45:45 -0600

    Added working dropdown boxes to get data and save it to appliation state

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, {Component} from 'react';
-import logo from './logo.svg';
 import './App.css';
 import ReactHighcharts from 'react-highcharts';
 
@@ -15,31 +14,92 @@ class App extends Component {
         super();
         this.api_key = 'pnpfgkjaqbt7vmx83zs2h9ub';
         this.httpInstance = axios.create({
-            baseURL: 'https://api.edmunds.com/api/',
+            baseURL: 'https://api.edmunds.com',
             timeout: 5000
         });
         this.httpInstance.interceptors.request.use((config) => {
             //Always send the api_key in the request
+            if (!config.params) {
+                config.params = {}
+            }
             config.params.api_key = this.api_key;
             return config;
         }, function (error) {
             console.log(error);
             return Promise.reject(error);
         });
+        this.state = {
+            appData: {
+                availableMakes: {},
+                availableModels: {}
+            },
+            selected: {
+                make: '*',
+                model: '*'
+            }
+        }
     }
 
     componentDidMount() {
-        // this.httpInstance.get('vehicle/v2/styles/count', {
-        //     params: {
-        //         state: "used"
-        //     }
-        // })
-        //     .then(function (response) {
-        //         console.log(response);
-        //     })
-        //     .catch(function (error) {
-        //         console.log(error);
-        //     });
+        this.httpInstance.get('/v1/api/tco/getmakeswithtcodata', {
+            params: {
+                fmt: 'json'
+            }
+        })
+            .then((response) => {
+                console.log(response);
+                this.setState({
+                    appData: Object.assign(this.state.appData, {availableMakes: response.data.makes})
+                });
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    }
+
+    changeModelHandler(e) {
+
+    }
+
+    changeMakeHandler(e) {
+        this.setState({
+                selected: Object.assign(this.state.selected, {make: e.target.value})
+            },
+            () => {
+                this.httpInstance.get('/v1/api/tco/getmodelswithtcodata', {
+                    params: {
+                        makeid: this.state.selected.make,
+                        fmt: 'json'
+                    }
+                })
+                    .then((response) => {
+                        console.log(response);
+                        this.setState({
+                            appData: Object.assign(this.state.appData, {availableModels: response.data.models})
+                        })
+                    })
+                    .catch((error) => {
+                        console.log(error);
+                    });
+            })
+    }
+
+    genMakeOptions() {
+        if (this.state.appData.availableMakes !== null) { //this waits for the data to come back and be assigned to `availableMakes`
+            return Object.keys(this.state.appData.availableMakes).map((key)=> {
+                let val = this.state.appData.availableMakes[key];
+                return <option value={val.id} key={key}>{val.name}</option>;
+            });
+        }
+    }
+
+    genModelOptions() {
+        if (this.state.appData.availableModels !== null) {
+            return Object.keys(this.state.appData.availableModels).map((key)=> {
+                let val = this.state.appData.availableModels[key];
+                return <option value={val.id} key={key}>{val.name}</option>;
+            });
+        }
     }
 
     render() {
@@ -53,6 +113,18 @@ class App extends Component {
         };
         return (
             <div className="container">
+                <div className="input-field col s12">
+                    <label>Make: </label>
+                    <select value={this.state.selected.make} onChange={e => this.changeMakeHandler(e)}>
+                        <option disabled value='*'>Select a Make</option>
+                        {this.genMakeOptions()}
+                    </select>
+                    <label>Model: </label>
+                    <select value={this.state.selected.model} onChange={e => this.changeModelHandler(e)}>
+                        <option disabled value='*'>Select a Model</option>
+                        {this.genModelOptions()}
+                    </select>
+                </div>
                 <div className="row">
                     <ReactHighcharts config={hc_config}/>
                 </div>
