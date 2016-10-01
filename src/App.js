094e04d248708b61cf23ed36d2847e6ec43e9c64
Darin Holbrook <dholbrook@overstock.com>
2016-09-30 19:27:31 -0600

    Pushing changes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
-import './App.css';
 import ReactHighcharts from 'react-highcharts';
+import update from 'react-addons-update';
+import './App.css';
 
 /* Choosing Axios because it is isomorphic - Both node.js and the client can use the same calls. Makes server-side validation a snap
  * Supports the Promise API without a polyfill
@@ -29,14 +30,19 @@ class App extends Component {
             return Promise.reject(error);
         });
         this.state = {
-            appData: {
-                availableMakes: {},
-                availableModels: {}
-            },
-            selected: {
-                make: '*',
-                model: '*'
-            }
+            // A flat state makes it easier to update and prevents unwanted mutations
+            // If you are working with a larger state USE REDUX
+            makes: {},
+            models: {},
+            yearsNew: [],
+            yearsUsed: [],
+
+            //Drop Down Selections
+            selectedMake: '*',
+            selectedMake_data: {},
+            selectedModel_data: {},
+            selectedModel: '*',
+            selectedYear: '*',
         }
     }
 
@@ -48,9 +54,7 @@ class App extends Component {
         })
             .then((response) => {
                 console.log(response);
-                this.setState({
-                    appData: Object.assign(this.state.appData, {availableMakes: response.data.makes})
-                });
+                this.setState({makes: response.data.makes});
             })
             .catch((error) => {
                 console.log(error);
@@ -58,24 +62,31 @@ class App extends Component {
     }
 
     changeModelHandler(e) {
-
+        this.setState({selectedModel: e.target.value},
+            () => {
+                let car = this.state.models[this.state.selectedModel];
+                if(car.years.NEW_USED){
+                    this.setState({yearsNew: car.years.NEW_USED});
+                }
+                if(car.years.USED){
+                    this.setState({yearsUsed: car.years.USED});
+                }
+            });
     }
 
     changeMakeHandler(e) {
-        this.setState({
-                selected: Object.assign(this.state.selected, {make: e.target.value})
-            },
+        this.setState({selectedMake: e.target.value},
             () => {
                 this.httpInstance.get('/v1/api/tco/getmodelswithtcodata', {
                     params: {
-                        makeid: this.state.selected.make,
+                        makeid: this.state.selectedMake,
                         fmt: 'json'
                     }
                 })
                     .then((response) => {
                         console.log(response);
                         this.setState({
-                            appData: Object.assign(this.state.appData, {availableModels: response.data.models})
+                            models: response.data.models
                         })
                     })
                     .catch((error) => {
@@ -84,20 +95,62 @@ class App extends Component {
             })
     }
 
+    changeYearsHandler(e){
+        this.setState({selectedYear: e.target.value},
+            () => {
+                this.httpInstance.get('/v1/api/tco/getstyleswithtcodatabysubmodel', {
+                    params: {
+                        make: this.state.models[this.state.selectedModel].nicemodel,
+                        model: this.state.models[this.state.selectedModel].nicemodel,
+                        year: this.state.selectedYear,
+                        submodel: this.state.models[this.state.selectedModel].submodel,
+                        fmt: 'json'
+                    }
+                })
+                    .then((response) => {
+                        console.log(response);
+                        this.setState({
+                            models: response.data.models
+                        })
+                    })
+                    .catch((error) => {
+                        console.log(error);
+                    });
+            });
+    }
+
     genMakeOptions() {
-        if (this.state.appData.availableMakes !== null) { //this waits for the data to come back and be assigned to `availableMakes`
-            return Object.keys(this.state.appData.availableMakes).map((key)=> {
-                let val = this.state.appData.availableMakes[key];
+        if (this.state.makes !== null) { //this waits for the data to come back and be assigned to `makes`
+            return Object.keys(this.state.makes).map((key)=> {
+                let val = this.state.makes[key];
                 return <option value={val.id} key={key}>{val.name}</option>;
             });
         }
     }
 
     genModelOptions() {
-        if (this.state.appData.availableModels !== null) {
-            return Object.keys(this.state.appData.availableModels).map((key)=> {
-                let val = this.state.appData.availableModels[key];
-                return <option value={val.id} key={key}>{val.name}</option>;
+        if (this.state.models !== null) {
+            return Object.keys(this.state.models).map((key)=> {
+                let val = this.state.models[key];
+                return <option value={key} key={key}>{val.name}</option>;
+            });
+        }
+    }
+
+    genYearsNewOptions(){
+        if (this.state.yearsNew !== null) {
+            return Object.keys(this.state.yearsNew).map((key)=> {
+                let val = this.state.yearsNew[key];
+                return <option value={val} key={key}>{val}</option>;
+            });
+        }
+    }
+
+    genYearsUsedOptions(){
+        if (this.state.yearsUsed !== null) {
+            return Object.keys(this.state.yearsUsed).map((key)=> {
+                let val = this.state.yearsUsed[key];
+                return <option value={val} key={key}>{val}</option>;
             });
         }
     }
@@ -115,14 +168,24 @@ class App extends Component {
             <div className="container">
                 <div className="input-field col s12">
                     <label>Make: </label>
-                    <select value={this.state.selected.make} onChange={e => this.changeMakeHandler(e)}>
+                    <select value={this.state.selectedMake} onChange={e => this.changeMakeHandler(e)}>
                         <option disabled value='*'>Select a Make</option>
                         {this.genMakeOptions()}
                     </select>
                     <label>Model: </label>
-                    <select value={this.state.selected.model} onChange={e => this.changeModelHandler(e)}>
+                    <select value={this.state.selectedModel} onChange={e => this.changeModelHandler(e)}>
                         <option disabled value='*'>Select a Model</option>
                         {this.genModelOptions()}
+                    </select>
+                    <label>New: </label>
+                    <select value={this.state.selectedYearsNew} onChange={e => this.changeYearsHandler(e)}>
+                        <option disabled value='*'>Select a Year - New</option>
+                        {this.genYearsNewOptions()}
+                    </select>
+                    <label>Used: </label>
+                    <select value={this.state.selectedYearsUsed} onChange={e => this.changeYearsHandler(e)}>
+                        <option disabled value='*'>Select a Year - Used</option>
+                        {this.genYearsUsedOptions()}
                     </select>
                 </div>
                 <div className="row">
